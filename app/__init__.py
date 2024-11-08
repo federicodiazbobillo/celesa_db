f92bd43182483d1033e17e822b1e04081189c8bc
Federico Diaz Bobillo <federicodiazbobillo@gmail.com>
2024-11-08 10:33:41 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,9 @@ from .controllers.routes import main
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
+    # Registra los blueprints
     app.register_blueprint(main)
+    app.register_blueprint(mercado_libre_bp)
+    app.register_blueprint(celesa_bp)
+    app.register_blueprint(configuracion_bp)
     return app
